6a7f6262f2f1c786939b3293ae31b8fae5d963e5
Olivér Falvai <ofalvai@gmail.com>
2017-01-30 15:49:06 +0100

    Stock list widget basics

---
+++ app/src/main/java/com/udacity/stockhawk/ui/widget/StockListWidgetProvider.java
@@ -0,0 +1,55 @@
+package com.udacity.stockhawk.ui.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.MainActivity;
+
+/*
+ * Copyright 2017. 01. 30. Olivér Falvai
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+public class StockListWidgetProvider extends AppWidgetProvider {
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_stock_list);
+            remoteViews.setOnClickPendingIntent(R.id.tv_hello_world, makePendingIntent(context));
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    private PendingIntent makePendingIntent(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        return PendingIntent.getActivity(context, 0, intent, 0);
+    }
+}
